bc4d964b78b391b1cd3018bfb7182ee1ff08124e
sachinsinghT <sachinsingh60051@gmail.com>
2025-11-03 12:16:27 +0530

    let car and console.log concept done

---
+++ Basic/varibles.js
@@ -0,0 +1,21 @@
+const accountId =1423;
+let userName ="john";
+var accountPassword = "123445";
+
+accountCity="jaipur";
+
+console.log(accountId);
+
+//accountId =43 // not allowed
+
+console.log(accountId);
+
+console.table([accountId,userName,accountPassword,accountCity]);
+
+
+// scope // use let or const only ...dont use var 
+
+
+let accountState;
+
+console.log(accountState);// undefined
