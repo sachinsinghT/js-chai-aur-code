30c0b44994e7d643fce5b884777a1f0ee6d0a4d9
sachinsinghT <sachinsingh60051@gmail.com>
2025-11-03 12:36:11 +0530

    datatype

---
+++ Basic/dataTypes.js
@@ -0,0 +1,18 @@
+// number > 2 to power 53
+// bigint 
+// string => ""
+// boolean => false
+// null => standalone value
+// undefined
+// symbol => Symbol() unique
+// object => {}
+
+let age=4;
+console.log(typeof age)
+
+console.log(typeof null);// object
+console.log(typeof undefined);
+console.log(typeof Symbol("id"));
+
+let bigNumber=1234567890123456789012345678901234567890n;
+console.log(typeof bigNumber);
