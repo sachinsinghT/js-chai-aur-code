50eb723213dea515550275ef18a0a56ff06cea54
sachinsinghT <sachinsingh60051@gmail.com>
2025-11-18 11:57:28 +0530

    basic completed

---
+++ Basic_02/reduce.js
@@ -0,0 +1,14 @@
+const myNums=[1,2,3]
+
+// const myTotal=myNums.reduce(function(acc,currval){
+//     console.log(`acc:${acc} and currval: ${currval}`)
+//     return acc+currval
+// },0)
+
+// console.log(myTotal)
+
+// using arrow function
+
+const myTotal=myNums.reduce((acc,currval)=>acc+currval,0)
+
+console.log(myTotal)
